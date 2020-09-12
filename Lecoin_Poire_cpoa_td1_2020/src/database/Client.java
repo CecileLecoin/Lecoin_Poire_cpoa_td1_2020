4bb2c9f1e7478ba23fea0bd1c4e36782c8d96e0b
CecileLecoin <45843292+CecileLecoin@users.noreply.github.com>
2020-09-12 10:23:44 +0200

    création classe client + get et set + tostring

---
+++ Lecoin_Poire_cpoa_td1_2020/src/database/Client.java
@@ -0,0 +1,33 @@
+package database;
+
+public class Client {
+	
+	private String nom, prenom;
+	
+	//getters et setters
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	//toStrings
+	@Override
+	public String toString() {
+		return "Client [nom=" + nom + ", prenom=" + prenom + "]";
+	}
+	
+	ResultSet res = requete.executeQuery("select nom, prenom from client");while (res.next()) {intno = res.getInt(1);String nom = res.getString("nom_etudiant");}
+	
+	
+}

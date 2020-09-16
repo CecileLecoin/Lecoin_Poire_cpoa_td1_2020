7b98cee14980ac5a10f379cfc0792b8d8b308f09
CecileLecoin <45843292+CecileLecoin@users.noreply.github.com>
2020-09-16 16:26:33 +0200

    oskour

--- Lecoin_Poire_cpoa_td1_2020/src/Main.java
+++ Lecoin_Poire_cpoa_td1_2020/src/Main.java
@@ -28,38 +28,39 @@ public class Main {
 						+ " -Ajouter un client : 'ajout'\n - Supprimer un client : 'suppr'\n -Modifier les informations d'un client : 'modif'\n");
 				choice="";
 				choice = sc.nextLine();
-				//sc.close();
 				choice.toLowerCase();
 				switch(choice) {
 					case "afficher la liste" : case "liste" : case "la liste":
 						ArrayList<Client> c= ClientSQL.listClient();
 						for(Client disp : c) {
-							System.out.println(disp.getPrenom() + " " + disp.getNom() + "\n");
+							System.out.println(disp.getIdClient() +disp.getPrenom() + " " + disp.getNom() + "\n");
 						}
 					break;
 					
 					case "afficher un client": case "client" : case "un client": 
 						System.out.println("Rentrez le numéro client \n");
 						int idC = sc.nextInt();
-						sc.close();
+
+						System.out.println(idC);
 						Client disp = ClientSQL.getById(idC);
 						System.out.println("Fiche info client : \n" + disp.getPrenom() + " " + disp.getNom());
 					break;
 					
 					case "ajouter un client": case "ajouter" : case "ajout": 
 						System.out.println("Nouveau client : \n");
+						
 						System.out.println("Nom : \n");
 						String a = sc.nextLine();
 						System.out.println("Prénom : \n");
 						String b = sc.nextLine();
 						// CODE A SUPPRIMER QD ON GERERA PLUS QUE JUSTE LE NOM ET LE PRENOM
-						String j = null;
-						String d = null;
-						String e = null;
-						String f = null;
-						String g = null;
-						String h = null;
-						String i =null;
+						String j = " ";
+						String d = " ";
+						String e = " ";
+						String f = " ";
+						String g = " ";
+						String h = " ";
+						String i = " ";
 						/*System.out.println("identifiant : \n");
 						String c = sc.nextLine();
 						System.out.println("Mot de passe : \n");
@@ -76,7 +77,9 @@ public class Main {
 						String i = sc.nextLine();*/
 						
 						sc.close();
-						Client clientAjoute = new Client(0, a, b, j, d, e, f,
+						ArrayList<Client> newC= ClientSQL.listClient();
+						int idNewC = newC.size()+1;
+						Client clientAjoute = new Client(idNewC, a, b, j, d, e, f,
 								g, h, i);
 						ClientSQL.ajoutClient(clientAjoute);
 						System.out.println("Fiche recap du nouveau client : \n" + a + " " + b);
@@ -84,43 +87,60 @@ public class Main {
 					
 					case "supprimer un client": case "supprimer" : case "suppr": 
 						System.out.println("Client à supprimer : \n");
-						System.out.println("Nom : \n");
-						String supN = sc.nextLine();
-						System.out.println("Prénom : \n");
-						String supP = sc.nextLine();
-						Client clientSupprime = new Client(0, supN, supP, null, null, null, null,
+						System.out.println("Num id client : \n");
+						int supN = sc.nextInt();
+						System.out.println("Supprimer ce client ? (o/n) \n");
+						Client dispC = ClientSQL.getById(supN);
+						System.out.println(dispC.getNom() + dispC.getPrenom());
+						String reponse = sc.nextLine();
+						if(reponse=="o")
+						{
+						Client clientSupprime = new Client(supN, " ", " ", null, null, null, null,
 								null, null, null);
 						ClientSQL.ajoutClient(clientSupprime);
-				}
+						}
+				
 				break;
 				
 			case "modifier un client": case "modifier" : case "modif": 
 				System.out.println("Numéro du client à modifier : \n");
-				int idC = sc.nextInt();
+				int idCm = sc.nextInt();
 				sc.close();
-				System.out.println("Nouveau nom : \n");
-				String a = sc.nextLine();
-				System.out.println("Nouveau prénom : \n");
-				String b = sc.nextLine();
-				/*System.out.println("identifiant : \n");
-				String c = sc.nextLine();
-				System.out.println("Mot de passe : \n");
-				String d = sc.nextLine();
-				System.out.println("Numero de rue : \n");
-				String e = sc.nextLine();
-				System.out.println("Voie : \n");
-				String f = sc.nextLine();
-				System.out.println("Code postal : \n");
-				String g = sc.nextLine();
-				System.out.println("Ville : \n");
-				String h = sc.nextLine();
-				System.out.println("Pays : \n");
-				String i = sc.nextLine();*/
-				
-				sc.close();
-				Client clientMod = ClientSQL.getById(idC);
-				ClientSQL.modifClient(clientMod, a, b);
-				System.out.println("Fiche recap du client : \n" + a + " " + b);
+
+				Client clientMod = ClientSQL.getById(idCm);
+				System.out.println("Supprimer ce client ? (oui/non) \n");
+				System.out.println(clientMod.getNom() + " " +clientMod.getPrenom()+"\n");
+				System.out.println("avant le scan ");
+				Scanner scan = new Scanner(System.in);
+				String reponseA = scan.nextLine();
+				System.out.println("après le scan ");
+
+				if(reponseA=="oui")
+				{
+					System.out.println("dans le if ");
+					System.out.println("Nouveau nom : \n");
+					String aM = sc.nextLine();
+					System.out.println("Nouveau prénom : \n");
+					String bM = sc.nextLine();
+					sc.close();
+					/*System.out.println("identifiant : \n");
+					String c = sc.nextLine();
+					System.out.println("Mot de passe : \n");
+					String d = sc.nextLine();
+					System.out.println("Numero de rue : \n");
+					String e = sc.nextLine();
+					System.out.println("Voie : \n");
+					String f = sc.nextLine();
+					System.out.println("Code postal : \n");
+					String g = sc.nextLine();
+					System.out.println("Ville : \n");
+					String h = sc.nextLine();
+					System.out.println("Pays : \n");
+					String i = sc.nextLine();*/
+				ClientSQL.modifClient(clientMod, aM, bM);
+				System.out.println("Fiche recap du client : \n" + aM + " " + bM);
+				}
+				System.out.println("après le if ");
 			break;
 				
 			case "categorie": case "categories": case "les categories":
@@ -130,7 +150,9 @@ public class Main {
 			default:
 				System.out.println("Selection non reconnue");
 				
+				
 		}
 	}
 
+}
 }
--- Lecoin_Poire_cpoa_td1_2020/src/database/ClientSQL.java
+++ Lecoin_Poire_cpoa_td1_2020/src/database/ClientSQL.java
@@ -45,12 +45,19 @@ public class ClientSQL {
 		Connexion connexion = new Connexion();
 		try {
 			Connection laConnexion = connexion.creeConnexion();
-			Statement requete = laConnexion.createStatement();
-			ResultSet res = requete.executeQuery("SELECT id_client, nom, prenom, identifiant, mot_de_passe, adr_numero, adr_voie, adr_code_postal, adr_ville, adr_pays FROM Client WHERE id_client="+ id);
-			client = new Client(res.getInt("id_client"), res.getString("nom"), res.getString("prenom"), res.getString("identifiant"), res.getString("mot_de_passe"), res.getString("adr_numero"),
-					res.getString("adr_voie"), res.getString("adr_code_postal"), res.getString("adr_ville"), res.getString("adr_pays"));
+			PreparedStatement requete = laConnexion.prepareStatement("SELECT * FROM Client WHERE id_client=?");
+			requete.setInt(1, id);
+			ResultSet res = requete.executeQuery();
+			while (res.next()) {
+				client = new Client(res.getInt("id_client"), res.getString("nom"), res.getString("prenom"), res.getString("identifiant"), res.getString("mot_de_passe"), res.getString("adr_numero"),
+						res.getString("adr_voie"), res.getString("adr_code_postal"), res.getString("adr_ville"), res.getString("adr_pays")); //null car pour le td1 on n'a pas besoin de gérer les autres élements à  part nom et prenom
+			}
+			if (res != null)
+				res.close();
 			if (requete != null)
 				requete.close();
+			if (laConnexion != null)
+				laConnexion.close();
 		} catch (SQLException sqle) {
 			System.out.println("Pb select" + sqle.getMessage());
 
@@ -64,20 +71,21 @@ public class ClientSQL {
 		Connexion connexion = new Connexion();
 		try {
 			Connection laConnexion = connexion.creeConnexion();
-			PreparedStatement requete = laConnexion.prepareStatement("insert into Client (nom, prenom) values (?, ?)", Statement.RETURN_GENERATED_KEYS);
+			PreparedStatement requete = laConnexion.prepareStatement("insert into Client (id_client, nom, prenom, identifiant, mot_de_passe, adr_numero, adr_voie, adr_code_postal, adr_ville, adr_pays) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
 
 			requete.setInt(1, cli.getIdClient());
 			requete.setString(2, cli.getNom());
 			requete.setString(3, cli.getPrenom());
-			/*requete.setString(4, cli.getIdentifiant());
+			requete.setString(4, cli.getIdentifiant());
 			requete.setString(5, cli.getMdp());
 			requete.setString(6, cli.getNum());
 			requete.setString(7, cli.getVoie());
 			requete.setString(8, cli.getCp());
 			requete.setString(9, cli.getVille());
-			requete.setString(10, cli.getPays());*/
+			requete.setString(10, cli.getPays());
 			requete.executeUpdate();
-			ResultSet res = requete.executeQuery();
+			ResultSet res = requete.getGeneratedKeys();
+			//System.out.println(res);
 			if (res != null)
 				res.close();
 			if (requete != null)
@@ -96,11 +104,9 @@ public class ClientSQL {
 		Connexion connexion = new Connexion();
 		try {
 			Connection laConnexion = connexion.creeConnexion();
-			PreparedStatement requete = laConnexion.prepareStatement("delete from Client where nom=? and prenom=?)", Statement.RETURN_GENERATED_KEYS);
+			PreparedStatement requete = laConnexion.prepareStatement("delete from Client where id_client=?)", Statement.RETURN_GENERATED_KEYS);
 
 			requete.setInt(1, cli.getIdClient());
-			requete.setString(2, cli.getNom());
-			requete.setString(3, cli.getPrenom());
 			requete.executeUpdate();
 			ResultSet res = requete.executeQuery();
 			if (res != null)

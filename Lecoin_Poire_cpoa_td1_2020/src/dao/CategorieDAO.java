d0fcc73348a340139d36b6ac1ddaaabfc47baefa
CecileLecoin <45843292+CecileLecoin@users.noreply.github.com>
2020-09-21 23:14:47 +0200

    il est 23h flm de preciser le comit

--- Lecoin_Poire_cpoa_td1_2020/src/dao/CategorieDAO.java
+++ Lecoin_Poire_cpoa_td1_2020/src/dao/CategorieDAO.java
@@ -5,13 +5,9 @@ import java.util.ArrayList;
 
 public interface CategorieDAO extends InterfaceDAO<Categorie> {
 
-    @Override
     public abstract Categorie getById(int id);
-    @Override
     public abstract boolean create(Categorie categorie);
-    @Override
     public abstract boolean update(Categorie categorie);
-    @Override
     public abstract boolean delete(Categorie categorie);
 
     public abstract ArrayList<Categorie> getByTitre(String titre);

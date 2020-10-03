69a10d0263236132da571cab619ab066f0f6de19
CecileLecoin <45843292+CecileLecoin@users.noreply.github.com>
2020-10-03 09:59:55 +0200

    c

--- Lecoin_Poire_cpoa_td1_2020/src/tests/MySQLCommandeDAOTest.java
+++ Lecoin_Poire_cpoa_td1_2020/src/tests/MySQLCommandeDAOTest.java
@@ -1,5 +1,32 @@
 package tests;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import dao.*;
+import metier.Commande;
+import metier.Produit;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class MySQLCommandeDAOTest {
-    
+
+    private Commande commande;
+
+	@Before
+	public void setUp() {
+		commande = new Commande();
+	}
+
+    @Test
+    public void testEquals() {
+		Commande commande2 = new Commande();
+		System.out.println(commande.toString());
+		System.out.println(commande2.toString());
+		assertEquals(commande, commande2);
+	}
+
 }

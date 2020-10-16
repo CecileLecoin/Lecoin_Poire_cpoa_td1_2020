d2360cb92ee8cc3ea9d1fc3ae821c076e72f95f6
poire13u <poire13u@iutm-inf-f36-21.iutmetz.site.univ-lorraine.fr>
2020-10-16 17:00:22 +0200

    Continuing of first windows product

--- src/graphique/controleur/CtrlProduits.java
+++ src/graphique/controleur/CtrlProduits.java
@@ -5,6 +5,7 @@ import daoFactory.DAOFactory;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import metier.Categorie;
 import metier.Produit;
@@ -12,7 +13,7 @@ import metier.Produit;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class CtrlProduits {
+public class CtrlProduits implements Initializable {
 
     @FXML
     private Label labelAffiche;
@@ -26,28 +27,48 @@ public class CtrlProduits {
     private TextField textFieldTarif;
     @FXML
     private TextField textFieldNom;
-
+    
+    private DAOFactory dao;
+    
+    @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        DAOFactory dao = DAOFactory.getDaoFactory(Persistence.LISTEMEMOIRE);
+        dao = DAOFactory.getDaoFactory(Persistence.LISTEMEMOIRE);
         dao.getCategorieDAO().create(new Categorie(0, "titre", "visuel"));
         this.choiceBoxCategorie.setItems(FXCollections.observableArrayList(dao.getCategorieDAO().findAll()));
     }
 
-    public void creerProduit(ActionEvent event) {
+    public void creerProduit() {
+    	
+    	Produit produit = new Produit();
+    	Categorie categorie = this.choiceBoxCategorie.getValue();
+    	
+    	try {
+    		String nom = this.textFieldNom.getText().trim();
+    		produit.setNom(nom);    		
+    	} catch (IllegalArgumentException e){
+    		this.labelAffiche.setText(e.getMessage());
+    	}
+    	try {
+    		String description = this.textAreaDescription.getText().trim();
+    		produit.setDescription(description);    		
+    	} catch (IllegalArgumentException e){
+    		this.labelAffiche.setText(e.getMessage());
+    	}
+    	try {
+    		float tarif = Float.parseFloat(this.textFieldTarif.getText().trim());
+    		produit.setTarif(tarif);    		
+    	} catch (IllegalArgumentException e){
+    		this.labelAffiche.setText(e.getMessage());
+    	}
+    	if (categorie == null) {
+    		this.labelAffiche.setText("Aucune catégorie séléctionner");
+    	}
+    	else {
+    		produit.setCategorie(categorie);    		
+    	}
 
-
-        Produit produit = new Produit();
-        String nom = this.textFieldNom.getText();
-        String description = this.textAreaDescription.getText();
-        float tarif = Float.parseFloat(this.textFieldTarif.getText());
-        Categorie categorie = this.choiceBoxCategorie.getValue();
-
-        produit.setDescription(description);
-        produit.setNom(nom);
-        produit.setTarif(tarif);
-        produit.setCategorie(categorie);
-
-        this.labelAffiche.setText(produit.toString());
+    	this.labelAffiche.setText(produit.toString());
+    	dao.getProduitDAO().create(produit);
     }
 }

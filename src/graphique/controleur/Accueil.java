36d77704bddf39c4dfe9fceba6170d4be56f64a1
CecileLecoin <45843292+CecileLecoin@users.noreply.github.com>
2020-10-28 13:59:29 +0100

    j'arrive pas à faire un event listener

--- src/graphique/controleur/Accueil.java
+++ src/graphique/controleur/Accueil.java
@@ -1,11 +1,10 @@
 package graphique.controleur;
 
-import javafx.collections.FXCollections;
-import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-import javafx.scene.Node;
+import javafx.scene.input.InputEvent;
 import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
@@ -16,16 +15,26 @@ public class Accueil implements Initializable {
     @FXML
     private Button btnChoix;
 
+    EventHandler handler = new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent event) {
+
+            String source1 = event.getSource().toString(); //yields complete string
+            Button btn = (Button) event.getSource();
+            String source2 = btn.getId();//returns JUST the id of the object that was clicked
+            System.out.println("Full String: " + source1);
+            System.out.println("Just the id: " + source2);
+            if (source2 == "btnClient") {
+                System.out.println("client sélectionné");
+            }
+        }
+    };
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
     }
 
-    public void btnClicked(ActionEvent event) {
-        //if (this.btnChoix.getId() == "btnCli") {
-        //    System.out.println("client séléctionné");
-        //}
-    }
 
     public void retourPage(MouseEvent mouseEvent) {
     }
---
+++ src/graphique/controleur/CtrlClients.java
@@ -0,0 +1,4 @@
+package graphique.controleur;
+
+public class CtrlClients {
+}
